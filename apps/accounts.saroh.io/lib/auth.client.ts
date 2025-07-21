e46a2f5556b17f6b94cf15cfb2ddefbc3fc54ffa
Mohit Mehta <mehtam2701@gmail.com>
2025-07-21 22:39:26 +0530

    updated auth schema and plugins

--- apps/accounts.saroh.io/lib/auth.client.ts
+++ apps/accounts.saroh.io/lib/auth.client.ts
@@ -1,6 +1,20 @@
+import {
+    adminClient,
+    apiKeyClient,
+    emailOTPClient,
+    organizationClient,
+    twoFactorClient,
+} from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     baseURL: process.env.BETTER_AUTH_URL as string,
+    plugins: [
+        emailOTPClient(),
+        adminClient(),
+        apiKeyClient(),
+        organizationClient(),
+        twoFactorClient()
+    ],
 });
 const { useSession } = authClient;
--- apps/accounts.saroh.io/lib/auth.ts
+++ apps/accounts.saroh.io/lib/auth.ts
@@ -2,10 +2,32 @@ import { PrismaClient } from "@prisma/client";
 // import prisma from "@/lib/prisma";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import {
+    admin,
+    anonymous,
+    apiKey,
+    emailOTP,
+    haveIBeenPwned,
+    multiSession,
+    organization,
+    twoFactor,
+    username,
+} from "better-auth/plugins";
 
 const prisma = new PrismaClient();
 
 export const auth = betterAuth({
+    account: {
+        accountLinking: {
+            enabled: true,
+        },
+    },
+    advanced: {
+        crossSubDomainCookies: {
+            enabled: true,
+            domain: ".saroh.io",
+        },
+    },
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
@@ -18,6 +40,24 @@ export const auth = betterAuth({
             clientSecret: process.env.AUTH_GITHUB_SECRET as string,
         },
     },
-    plugins: [],
+    plugins: [
+        admin(),
+        organization(),
+        apiKey(),
+        username(),
+        anonymous(),
+        haveIBeenPwned(),
+        twoFactor(),
+        multiSession(),
+        emailOTP({
+            sendVerificationOTP: async (email, token) => {
+                console.log(email, token);
+            },
+        }),
+    ],
     baseURL: process.env.BETTER_AUTH_URL as string,
+    rateLimit:{
+        storage: "database",
+        modelName: "rate_limit",
+    }
 });
